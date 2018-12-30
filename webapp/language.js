0438cfb2112680b6c03d5a6256d57edef19a93ae
刘循光 <1069899238@qq.com>
2018-12-30 13:43:23 +0800

    修改代码

--- webapp/language.js
+++ webapp/language.js
@@ -5,12 +5,27 @@ $(document).ready(function(){
     var en = "English";
     $("#btnLanguage").text(wu);
 
-    var p1En = "zzz Model Agency is an international model management agency based in China(Chengdu).It was established at beginning of 2015. Members of team come from media, advertising, public relations ";
-    var p1Wu = "";
+    var pWonderEn = "Wonder Model Agency is an international model management agency based in China(Chengdu).It was established at beginning of 2015. Members of team come from media, advertising, public relations ";
+    var pWonderWu = "乌克兰语";
+
+    var pSinceEn = "Since inception, Wonder Model Agency has remained true to its core values: change has two sides, opportunity and risk.We using unique connections and industry knowledge to develop and nurture models. faceto risk, hold the opportunity";
+    var pSinceWu = "乌克兰语";
+
+
+
+
 
 
     $("#btnLanguage").click(function(){
-        $("#btnLanguage").text(wu);
-        $("#p1").text(p1En);
+        if(($("#btnLanguage").text() == wu)){
+            $("#btnLanguage").text(en);
+            $("#pWonder").text(pWonderWu);
+            $("#pSince").text(pSinceWu);
+
+        } else {
+            $("#btnLanguage").text(wu);
+            $("#pWonder").text(pWonderEn);
+            $("#pSince").text(pSinceEn);
+        }
     });
 });
